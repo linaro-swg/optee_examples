6a2e705793c965198e788f4a0d2798d488582a2e
Jerome Forissier <jerome.forissier@linaro.org>
2018-03-12 13:52:51 +0100

    [Review] [RFC] Add example TA with three libraries, one static and two shared
    Signed-off-by: Jerome Forissier <jerome.forissier@linaro.org>

--- talibs/ta/user_ta_header_defines.h
+++ talibs/ta/user_ta_header_defines.h
@@ -30,7 +30,7 @@
 /* Extra properties (give a version id and a string name) */
 #define TA_CURRENT_TA_EXT_PROPERTIES \
     { "gp.ta.description", USER_TA_PROP_TYPE_STRING, \
-        "Example of OP-TEE Hello World Trusted Application" }, \
+        "Trusted Application using multiple user-defined libraries" }, \
     { "gp.ta.version", USER_TA_PROP_TYPE_U32, &(const uint32_t){ 0x0010 } }
 
 #endif /* USER_TA_HEADER_DEFINES_H */
