dbb378542f51eba731892f50860f3fea3a58cb02
Victor Chong <victor.chong@linaro.org>
2021-05-06 08:46:12 +0100

    plugins/Android.mk: add segment to build plugin
    Add Makefile segment to build the plugin module.
    
    Signed-off-by: Victor Chong <victor.chong@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- plugins/Android.mk
+++ plugins/Android.mk
@@ -1,6 +1,6 @@
-###################### optee-plugins ######################
 LOCAL_PATH := $(call my-dir)
 
+###################### optee-plugins ######################
 include $(CLEAR_VARS)
 LOCAL_CFLAGS += -DANDROID_BUILD
 LOCAL_CFLAGS += -Wall
@@ -15,4 +15,30 @@ LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_EXECUTABLE)
 
+###################### optee-plugins libs #################
+include $(CLEAR_VARS)
+
+PLUGIN_UUID = 96bcf744-4f72-4866-bf1d-8634fd9c65e5
+
+PLUGIN                  = $(PLUGIN_UUID).plugin
+PLUGIN_INCLUDES_DIR     = $(LOCAL_PATH)/syslog
+
+LOCAL_MODULE := $(PLUGIN)
+LOCAL_MODULE_RELATIVE_PATH := tee-supplicant/plugins
+LOCAL_VENDOR_MODULE := true
+# below is needed to locate optee_client exported headers
+LOCAL_SHARED_LIBRARIES := libteec
+
+LOCAL_SRC_FILES += syslog/syslog_plugin.c
+LOCAL_C_INCLUDES += $(PLUGIN_INCLUDES_DIR)
+
+LOCAL_MODULE_TAGS := optional
+
+# Build the 32-bit and 64-bit versions.
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TARGET_ARCH := arm arm64
+
+include $(BUILD_SHARED_LIBRARY)
+
+###################### TA #################################
 include $(LOCAL_PATH)/ta/Android.mk
