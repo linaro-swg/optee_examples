88bf8a334fa3cb3bcf283b05bea0d7de2eb9ee34
Victor Chong <victor.chong@linaro.org>
2021-05-06 08:37:08 +0100

    Android.mk: remove OPTEE_CLIENT_EXPORT
    OPTEE_CLIENT_EXPORT is not required in Android.mk. The AOSP build does
    not create an out/export dir.
    
    Signed-off-by: Victor Chong <victor.chong@linaro.org>
    Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

--- secure_storage/Android.mk
+++ secure_storage/Android.mk
@@ -1,15 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
-OPTEE_CLIENT_EXPORT ?= $(LOCAL_PATH)/../../optee_client/out/export
-
 include $(CLEAR_VARS)
 LOCAL_CFLAGS += -DANDROID_BUILD
 LOCAL_CFLAGS += -Wall
 
 LOCAL_SRC_FILES += host/main.c
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/ta/include \
-		    $(OPTEE_CLIENT_EXPORT)/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/ta/include
 
 LOCAL_SHARED_LIBRARIES := libteec
 LOCAL_MODULE := optee_example_secure_storage
