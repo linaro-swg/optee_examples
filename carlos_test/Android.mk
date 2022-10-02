7e3754efe61ab2b14ca45efd868ea573532e159e
Carlos Wei <carlos.wei.hk@gmail.com>
2022-10-02 17:42:50 +0800

    [carlos_test] add carlos_test
    Signed-off-by: Carlos Wei <carlos.wei.hk@gmail.com>

---
+++ carlos_test/Android.mk
@@ -0,0 +1,18 @@
+###################### optee-hello-world ######################
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_CFLAGS += -DANDROID_BUILD
+LOCAL_CFLAGS += -Wall
+
+LOCAL_SRC_FILES += host/main.c
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/ta/include
+
+LOCAL_SHARED_LIBRARIES := libteec
+LOCAL_MODULE := optee_example_carlos_test
+LOCAL_VENDOR_MODULE := true
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
+
+include $(LOCAL_PATH)/ta/Android.mk
