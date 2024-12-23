c494ae8263d466b8a13907cb52d46b6a99553b79
ivila <390810839@qq.com>
2024-12-23 15:09:14 +0800

    Remove misleading TA properties in new added examples
    Some of our example TAs define the following properties:
     - "gp.ta.description" (type: string)
     - "gp.ta.version" (type: unsigned 32-bit integer)
    
    This is misleading, because these are *not* the standard description and
    version properties defined by the GP Internal Core API specification. The
    standard ones are:
     - "gpd.ta.description" (type: string)
     - "gpd.ta.version" (type: string)
    OP-TEE TAs should use the TA_DESCRIPTION and TA_VERSION macros to define
    them.
    
    This commit gets rid of the misleading properties. Instead, TA_DESCRIPTION
    and TA_VERSION are set in the applications.
    
    Signed-off-by: ivila <390810839@qq.com>
    Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
    Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>

--- acipher/ta/user_ta_header_defines.h
+++ acipher/ta/user_ta_header_defines.h
@@ -15,12 +15,17 @@
 #define TA_UUID				TA_ACIPHER_UUID
 
 #define TA_FLAGS			TA_FLAG_EXEC_DDR
+
+/* Provisioned stack size */
 #define TA_STACK_SIZE			(2 * 1024)
+
+/* Provisioned heap size for TEE_Malloc() and friends */
 #define TA_DATA_SIZE			(32 * 1024)
 
-#define TA_CURRENT_TA_EXT_PROPERTIES \
-    { "gp.ta.description", USER_TA_PROP_TYPE_STRING, \
-        "Example of TA using asymmetric cipher" }, \
-    { "gp.ta.version", USER_TA_PROP_TYPE_U32, &(const uint32_t){ 0x0010 } }
+/* The gpd.ta.version property */
+#define TA_VERSION	"1.0"
+
+/* The gpd.ta.description property */
+#define TA_DESCRIPTION	"Example of TA using asymmetric cipher"
 
 #endif /*USER_TA_HEADER_DEFINES_H*/
--- aes/ta/user_ta_header_defines.h
+++ aes/ta/user_ta_header_defines.h
@@ -37,12 +37,17 @@
 #define TA_UUID				TA_AES_UUID
 
 #define TA_FLAGS			TA_FLAG_EXEC_DDR
+
+/* Provisioned stack size */
 #define TA_STACK_SIZE			(2 * 1024)
+
+/* Provisioned heap size for TEE_Malloc() and friends */
 #define TA_DATA_SIZE			(32 * 1024)
 
-#define TA_CURRENT_TA_EXT_PROPERTIES \
-    { "gp.ta.description", USER_TA_PROP_TYPE_STRING, \
-        "Example of TA using an AES sequence" }, \
-    { "gp.ta.version", USER_TA_PROP_TYPE_U32, &(const uint32_t){ 0x0010 } }
+/* The gpd.ta.version property */
+#define TA_VERSION	"1.0"
+
+/* The gpd.ta.description property */
+#define TA_DESCRIPTION	"Example of TA using an AES sequence"
 
 #endif /*USER_TA_HEADER_DEFINES_H*/
