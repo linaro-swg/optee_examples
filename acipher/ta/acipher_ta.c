linaro-swg/optee_examples

commit 934c7edb74a26e90f68024cf441073528444177f

acipher: Add dynamic algorithm selection and decryption support
Add support for new RSA PKCS OAEP algorithms:
- TEE_ALG_RSAES_PKCS1_V1_5
- TEE_ALG_RSAES_PKCS1_OAEP_MGF1_SHA1
- TEE_ALG_RSAES_PKCS1_OAEP_MGF1_SHA224
- TEE_ALG_RSAES_PKCS1_OAEP_MGF1_SHA256
- TEE_ALG_RSAES_PKCS1_OAEP_MGF1_SHA384
- TEE_ALG_RSAES_PKCS1_OAEP_MGF1_SHA512

Also add support to select algorithm at runtime. The user can now run
the `optee_example_acipher <key_size> <string to encrypt> <algo name>`
command, and the specified algorithm will be used during
encryption/decryption.

The user can now invoke:
optee_example_acipher <key_size> <string to encrypt> <algo name>

Supported values for <key_size> are:
- 2048 bits
- 3072 bits
- 4096 bits

Supported values for <algo name> are:
- TA_ALG_PKCS1_V1_5
- TA_ALG_OAEP_MGF1_SHA1
- TA_ALG_OAEP_MGF1_SHA224
- TA_ALG_OAEP_MGF1_SHA256
- TA_ALG_OAEP_MGF1_SHA384
- TA_ALG_OAEP_MGF1_SHA512

If no algorithm is specified, TA_ALG_PKCS1_V1_5 is selected by default.

Based on the input, the corresponding algorithm is selected and used for
encryption/decryption operations. This enhancement improves flexibility
by allowing users to test different RSA modes using a single binary.

Signed-off-by: Amey Avinash Raghatate <ameyavinash.raghatate@amd.com>
Reviewed-by: Etienne Carriere <etienne.carriere@foss.st.com>
Acked-by: Jerome Forissier <jerome.forissier@linaro.org>

acipher/ta/acipher_ta.c
@@ -32,7 +32,8 @@ static TEE_Result cmd_gen_key(struct acipher *state, uint32_t pt,
 
 	res = TEE_AllocateTransientObject(key_type, key_size, &key);
 	if (res) {
-		EMSG("TEE_AllocateTransientObject(%#" PRIx32 ", %" PRId32 "): %#" PRIx32, key_type, key_size, res);
+		EMSG("TEE_AllocateTransientObject(%#" PRIx32 ", %" PRId32 "): %#"
+		     PRIx32, key_type, key_size, res);
 		return res;
 	}
 
@@ -57,13 +58,14 @@ static TEE_Result cmd_enc(struct acipher *state, uint32_t pt,
 	uint32_t inbuf_len = 0;
 	void *outbuf = NULL;
 	uint32_t outbuf_len = 0;
+	uint32_t alg_num = 0;
 	TEE_OperationHandle op = TEE_HANDLE_NULL;
 	TEE_ObjectInfo key_info = { };
-	const uint32_t alg = TEE_ALG_RSAES_PKCS1_V1_5;
+	uint32_t encrypt = 0;
 	const uint32_t exp_pt = TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INPUT,
 						TEE_PARAM_TYPE_MEMREF_OUTPUT,
-						TEE_PARAM_TYPE_NONE,
-						TEE_PARAM_TYPE_NONE);
+						TEE_PARAM_TYPE_VALUE_INPUT,
+						TEE_PARAM_TYPE_VALUE_INPUT);
 
 	if (pt != exp_pt)
 		return TEE_ERROR_BAD_PARAMETERS;
@@ -80,11 +82,25 @@ static TEE_Result cmd_enc(struct acipher *state, uint32_t pt,
 	inbuf_len = params[0].memref.size;
 	outbuf = params[1].memref.buffer;
 	outbuf_len = params[1].memref.size;
+	alg_num = params[3].value.a;
 
-	res = TEE_AllocateOperation(&op, alg, TEE_MODE_ENCRYPT,
+	if (params[2].value.a)
+		encrypt = TEE_MODE_ENCRYPT;
+	else
+		encrypt = TEE_MODE_DECRYPT;
+
+	res = TEE_AllocateOperation(&op, alg_num, encrypt,
 				    key_info.keySize);
 	if (res) {
-		EMSG("TEE_AllocateOperation(TEE_MODE_ENCRYPT, %#" PRIx32 ", %" PRId32 "): %#" PRIx32, alg, key_info.keySize, res);
+		if (encrypt == TEE_MODE_ENCRYPT) {
+			EMSG("TEE_AllocateOperation(TEE_MODE_ENCRYPT, %#"
+			     PRIx32 ", %" PRId32 "): %#" PRIx32,
+			     alg_num, key_info.keySize, res);
+		} else {
+			EMSG("TEE_AllocateOperation(TEE_MODE_DECRYPT, %#"
+			     PRIx32 ", %" PRId32 "): %#" PRIx32,
+			     alg_num, key_info.keySize, res);
+		}
 		return res;
 	}
 
@@ -94,10 +110,22 @@ static TEE_Result cmd_enc(struct acipher *state, uint32_t pt,
 		goto out;
 	}
 
-	res = TEE_AsymmetricEncrypt(op, NULL, 0, inbuf, inbuf_len, outbuf,
-				    &outbuf_len);
-	if (res) {
-		EMSG("TEE_AsymmetricEncrypt(%" PRId32 ", %" PRId32 "): %#" PRIx32, inbuf_len, params[1].memref.size, res);
+	if (encrypt == TEE_MODE_ENCRYPT) {
+		res = TEE_AsymmetricEncrypt(op, NULL, 0, inbuf,
+					    inbuf_len, outbuf, &outbuf_len);
+		if (res) {
+			EMSG("TEE_AsymmetricEncrypt(%" PRId32 ", %"
+			     PRId32 "): %#" PRIx32, inbuf_len,
+			     params[1].memref.size, res);
+		}
+	} else {
+		res = TEE_AsymmetricDecrypt(op, NULL, 0, inbuf, inbuf_len,
+					    outbuf, &outbuf_len);
+		if (res) {
+			EMSG("TEE_AsymmetricDecrypt(%" PRId32 ", %"
+			     PRId32 "): %#" PRIx32, inbuf_len,
+			     params[1].memref.size, res);
+		}
 	}
 	params[1].memref.size = outbuf_len;
 
@@ -153,7 +181,7 @@ TEE_Result TA_InvokeCommandEntryPoint(void *session, uint32_t cmd,
 	switch (cmd) {
 	case TA_ACIPHER_CMD_GEN_KEY:
 		return cmd_gen_key(session, param_types, params);
-	case TA_ACIPHER_CMD_ENCRYPT:
+	case TA_ACIPHER_CMD_ENCRYPT_DECRYPT:
 		return cmd_enc(session, param_types, params);
 	default:
 		EMSG("Command ID %#" PRIx32 " is not supported", cmd);

acipher/host/main.c
@@ -16,26 +16,36 @@
 /* For the UUID (found in the TA's h-file(s)) */
 #include <acipher_ta.h>
 
+#define ENCRYPT         1
+#define DECRYPT         0
+
 static void usage(int argc, char *argv[])
 {
 	const char *pname = argv[0];
 
-	fprintf(stderr, "usage: %s <key_size> <string to encrypt>\n\n", pname);
-	fprintf(stderr, "Example of RSAES PKCS#1 v1.5 asymmetric encryption in a TA\n\n");
-	fprintf(stderr, "<key_size>             Key size in bits\n");
-	fprintf(stderr, "<string to encrypt>    String to encrypt\n");
+	fprintf(stderr, "Usage: %s <key_size> <string to encrypt> [<algo name>]\n\n",
+		pname);
+	fprintf(stderr, "<key_size>:  key size in bits. Supported values are:\n");
+	fprintf(stderr, "             2048, 3072, 4096\n");
+	fprintf(stderr, "<algo_name>: algorithm name. Supported values are:\n");
+	fprintf(stderr, "             TA_ALG_PKCS1_V1_5 (default)\n");
+	fprintf(stderr, "             TA_ALG_OAEP_MGF1_SHA1\n");
+	fprintf(stderr, "             TA_ALG_OAEP_MGF1_SHA224\n");
+	fprintf(stderr, "             TA_ALG_OAEP_MGF1_SHA256\n");
+	fprintf(stderr, "             TA_ALG_OAEP_MGF1_SHA384\n");
+	fprintf(stderr, "             TA_ALG_OAEP_MGF1_SHA512\n");
 	exit(1);
 }
 
 static void get_args(int argc, char *argv[], size_t *key_size, void **inbuf,
-		     size_t *inbuf_len)
+		     size_t *inbuf_len, uint32_t *algo_num)
 {
 	char *ep;
 	long ks;
+	char *algo;
 
-	if (argc != 3) {
-		warnx("Unexpected number of arguments %d (expected 2)",
-		      argc - 1);
+	if (argc < 3 || argc > 4) {
+		warnx("Unexpected number of arguments %d", argc - 1);
 		usage(argc, argv);
 	}
 
@@ -52,6 +62,32 @@ static void get_args(int argc, char *argv[], size_t *key_size, void **inbuf,
 
 	*inbuf = argv[2];
 	*inbuf_len = strlen(argv[2]);
+
+	if (argc > 3) {
+		algo = argv[3];
+		printf("%s algo selected\n", algo);
+		if (strcmp(algo, "TA_ALG_OAEP_MGF1_SHA1") == 0) {
+			*algo_num = TA_ALG_OAEP_MGF1_SHA1;
+		} else if (strcmp(algo, "TA_ALG_OAEP_MGF1_SHA224") == 0) {
+			*algo_num = TA_ALG_OAEP_MGF1_SHA224;
+		} else if (strcmp(algo, "TA_ALG_OAEP_MGF1_SHA256") == 0) {
+			*algo_num = TA_ALG_OAEP_MGF1_SHA256;
+		} else if (strcmp(algo, "TA_ALG_OAEP_MGF1_SHA384") == 0) {
+			*algo_num = TA_ALG_OAEP_MGF1_SHA384;
+		} else if (strcmp(algo, "TA_ALG_OAEP_MGF1_SHA512") == 0) {
+			*algo_num = TA_ALG_OAEP_MGF1_SHA512;
+		} else if (strcmp(algo, "TA_ALG_PKCS1_V1_5") == 0) {
+			*algo_num = TA_ALG_PKCS1_V1_5;
+		} else {
+			fprintf(stderr, "%s algo is invalid\n", algo);
+			usage(argc, argv);
+		}
+	} else {
+		printf("TA_ALG_PKCS1_V1_5 algo selected\n");
+		*algo_num = TA_ALG_PKCS1_V1_5;
+	}
+
+
 }
 
 static void teec_err(TEEC_Result res, uint32_t eo, const char *str)
@@ -69,10 +105,13 @@ int main(int argc, char *argv[])
 	size_t key_size;
 	void *inbuf;
 	size_t inbuf_len;
+	void *outbuf = NULL;
+	size_t outbuf_len = 0;
 	size_t n;
+	uint32_t algo_num;
 	const TEEC_UUID uuid = TA_ACIPHER_UUID;
 
-	get_args(argc, argv, &key_size, &inbuf, &inbuf_len);
+	get_args(argc, argv, &key_size, &inbuf, &inbuf_len, &algo_num);
 
 	res = TEEC_InitializeContext(NULL, &ctx);
 	if (res)
@@ -95,26 +134,62 @@ int main(int argc, char *argv[])
 	memset(&op, 0, sizeof(op));
 	op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_INPUT,
 					 TEEC_MEMREF_TEMP_OUTPUT,
-					 TEEC_NONE, TEEC_NONE);
+					 TEEC_VALUE_INPUT, TEEC_VALUE_INPUT);
 	op.params[0].tmpref.buffer = inbuf;
 	op.params[0].tmpref.size = inbuf_len;
+	op.params[2].value.a = ENCRYPT; /* encrypt */
+	op.params[3].value.a = algo_num;
 
-	res = TEEC_InvokeCommand(&sess, TA_ACIPHER_CMD_ENCRYPT, &op, &eo);
+	res = TEEC_InvokeCommand(&sess, TA_ACIPHER_CMD_ENCRYPT_DECRYPT, &op, &eo);
 	if (eo != TEEC_ORIGIN_TRUSTED_APP || res != TEEC_ERROR_SHORT_BUFFER)
-		teec_err(res, eo, "TEEC_InvokeCommand(TA_ACIPHER_CMD_ENCRYPT)");
+		teec_err(res, eo, "Command TA_ACIPHER_CMD_ENCRYPT_DECRYPT failed for encryption");
+
+	outbuf_len = op.params[1].tmpref.size;
+	op.params[1].tmpref.buffer = malloc(op.params[1].tmpref.size);
+	if (!op.params[1].tmpref.buffer)
+		err(1, "Cannot allocate out buffer of size %zu",
+		    outbuf_len);
+
+	res = TEEC_InvokeCommand(&sess, TA_ACIPHER_CMD_ENCRYPT_DECRYPT, &op, &eo);
+	if (res)
+		teec_err(res, eo, "Command TA_ACIPHER_CMD_ENCRYPT_DECRYPT failed for encryption");
+
+	outbuf = malloc(outbuf_len);
+	if (!outbuf)
+		err(1, "Cannot allocate out buffer of size %zu", outbuf_len);
+
+	memmove(outbuf, op.params[1].tmpref.buffer, outbuf_len);
+	printf("Encrypted buffer: ");
+	for (n = 0; n < outbuf_len; n++)
+		printf("%02x ", ((uint8_t *)outbuf)[n]);
+	printf("\n");
+
+	memset(&op, 0, sizeof(op));
+	op.paramTypes = TEEC_PARAM_TYPES(TEEC_MEMREF_TEMP_INPUT,
+					 TEEC_MEMREF_TEMP_OUTPUT,
+					 TEEC_VALUE_INPUT, TEEC_VALUE_INPUT);
+	op.params[0].tmpref.buffer = outbuf;
+	op.params[0].tmpref.size = outbuf_len;
+	op.params[2].value.a = DECRYPT; /* decrypt */
+	op.params[3].value.a = algo_num;
+
+	res = TEEC_InvokeCommand(&sess, TA_ACIPHER_CMD_ENCRYPT_DECRYPT, &op, &eo);
+	if (eo != TEEC_ORIGIN_TRUSTED_APP || res != TEEC_ERROR_SHORT_BUFFER)
+		teec_err(res, eo, "Command TA_ACIPHER_CMD_ENCRYPT_DECRYPT failed for decryption");
 
 	op.params[1].tmpref.buffer = malloc(op.params[1].tmpref.size);
 	if (!op.params[1].tmpref.buffer)
 		err(1, "Cannot allocate out buffer of size %zu",
-		    op.params[1].tmpref.size);
+		    outbuf_len);
 
-	res = TEEC_InvokeCommand(&sess, TA_ACIPHER_CMD_ENCRYPT, &op, &eo);
+	res = TEEC_InvokeCommand(&sess, TA_ACIPHER_CMD_ENCRYPT_DECRYPT, &op, &eo);
 	if (res)
-		teec_err(res, eo, "TEEC_InvokeCommand(TA_ACIPHER_CMD_ENCRYPT)");
+		teec_err(res, eo, "Command TA_ACIPHER_CMD_ENCRYPT_DECRYPT failed for decryption");
 
-	printf("Encrypted buffer: ");
-	for (n = 0; n < op.params[1].tmpref.size; n++)
-		printf("%02x ", ((uint8_t *)op.params[1].tmpref.buffer)[n]);
-	printf("\n");
+	if (memcmp(inbuf, op.params[1].tmpref.buffer, op.params[1].tmpref.size))
+		printf("message is not matching\n");
+	else
+		printf("message is matching successfully\n");
+
 	return 0;
 }

acipher/ta/include/acipher_ta.h
@@ -17,9 +17,19 @@
 #define TA_ACIPHER_CMD_GEN_KEY		0
 
 /*
- * in	params[1].memref  input
- * out	params[2].memref  output
+ * in	params[0].memref  Input data to cipher
+ * out	params[1].memref  Ciphered output data
+ * in   params[2].value.a  Mode: 0 for decryption, any other value for
+ * encryption
+ * in   params[3].value.a  Algorithm (TA_ALG_*)
  */
-#define TA_ACIPHER_CMD_ENCRYPT		1
+#define TA_ACIPHER_CMD_ENCRYPT_DECRYPT	1
+
+#define TA_ALG_PKCS1_V1_5		0x60000130
+#define TA_ALG_OAEP_MGF1_SHA1		0x60210230
+#define TA_ALG_OAEP_MGF1_SHA224		0x60310230
+#define TA_ALG_OAEP_MGF1_SHA256		0x60410230
+#define TA_ALG_OAEP_MGF1_SHA384		0x60510230
+#define TA_ALG_OAEP_MGF1_SHA512		0x60610230
 
 #endif /* __ACIPHER_TA_H */

commit aa698dd6c982df195c4f0616a581462647ca1be9

acipher: ta: handle NULL output buffer in decrypt size-probe
The two-phase size-probe pattern for encryption works by passing a NULL
output buffer on the first call; crypto_acipher_rsaes_encrypt() detects
that the reported output size is smaller than the key modulus and returns
TEE_ERROR_SHORT_BUFFER before inspecting the pointer itself.

The decryption path does not follow the same pattern.
crypto_acipher_rsaes_decrypt() validates all pointer arguments up front:

  if (!key || !msg || !cipher || !msg_len || ...)
          return TEE_ERROR_BAD_PARAMETERS;

A NULL output pointer therefore returns TEE_ERROR_BAD_PARAMETERS before
the required size can be computed, and the host receives
TEEC_ERROR_BAD_PARAMETERS instead of the expected
TEEC_ERROR_SHORT_BUFFER, causing a fatal error on the first probe call.

Fix in the TA: when outbuf is NULL, allocate a temporary internal
buffer of key-modulus size, perform the full decryption into it to
resolve the exact plaintext length, free the buffer, write the required
size into params[1].memref.size, and return TEE_ERROR_SHORT_BUFFER.

Fixes: 934c7ed ("acipher: Add dynamic algorithm selection and decryption support")

Signed-off-by: Shubham Soni <shubham.manojsoni@amd.com>
Signed-off-by: Akshay Belsare <akshay.belsare@amd.com>
Acked-by: Jerome Forissier <jerome.forissier@arm.com>

acipher/ta/acipher_ta.c
@@ -119,12 +119,46 @@ static TEE_Result cmd_enc(struct acipher *state, uint32_t pt,
 			     params[1].memref.size, res);
 		}
 	} else {
-		res = TEE_AsymmetricDecrypt(op, NULL, 0, inbuf, inbuf_len,
-					    outbuf, &outbuf_len);
-		if (res) {
-			EMSG("TEE_AsymmetricDecrypt(%" PRId32 ", %"
-			     PRId32 "): %#" PRIx32, inbuf_len,
-			     params[1].memref.size, res);
+		if (!outbuf) {
+			/*
+			 * Size-probe call: the host passes a NULL output buffer
+			 * to query the required plaintext length.
+			 * TEE_AsymmetricDecrypt() rejects a NULL destination
+			 * pointer with TEE_ERROR_BAD_PARAMETERS before it can
+			 * compute the output size.  Allocate a temporary
+			 * internal buffer sized to the maximum RSA output
+			 * (key modulus bytes), run the full decryption into it
+			 * to obtain the exact plaintext length, then free it
+			 * and return TEE_ERROR_SHORT_BUFFER so the host can
+			 * re-invoke with a correctly-sized output buffer.
+			 */
+			uint8_t *tmp_buf = NULL;
+
+			outbuf_len = key_info.keySize / 8;
+			tmp_buf = TEE_Malloc(outbuf_len, 0);
+			if (!tmp_buf) {
+				res = TEE_ERROR_OUT_OF_MEMORY;
+				goto out;
+			}
+			res = TEE_AsymmetricDecrypt(op, NULL, 0, inbuf,
+						    inbuf_len, tmp_buf,
+						    &outbuf_len);
+			TEE_Free(tmp_buf);
+			if (res != TEE_SUCCESS) {
+				EMSG("TEE_AsymmetricDecrypt size probe failed: %#"
+				     PRIx32, res);
+				goto out;
+			}
+			res = TEE_ERROR_SHORT_BUFFER;
+		} else {
+			res = TEE_AsymmetricDecrypt(op, NULL, 0, inbuf,
+						    inbuf_len, outbuf,
+						    &outbuf_len);
+			if (res) {
+				EMSG("TEE_AsymmetricDecrypt(%" PRId32 ", %"
+				     PRId32 "): %#" PRIx32, inbuf_len,
+				     params[1].memref.size, res);
+			}
 		}
 	}
 	params[1].memref.size = outbuf_len;